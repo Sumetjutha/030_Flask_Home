7d5a73b13116e1315f86b825ab6901f11fd3e7c6
Sumet Jutajan <sumet.jutha@gmail.com>
2023-01-26 23:52:23 +0700

    update and create

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,23 @@ stores = [
     }
 ]
 
-@app.get("/store")
+@app.get("/store") # http://127.0.0.1:5000/store
 def get_stores():
     return {"stores": stores}
+
+@app.post("/store") # http://127.0.0.1:5000/store
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
